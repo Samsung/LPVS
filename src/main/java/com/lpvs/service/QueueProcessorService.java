719094793bc2ea57d51a7420b10ee3c4c1924032
Oleg Kopysov <o.kopysov@samsung.com>
2022-08-17 11:23:30 +0300

    Updated corporation name
    Signed-off-by: Oleg Kopysov <o.kopysov@samsung.com>

--- src/main/java/com/lpvs/service/QueueProcessorService.java
+++ src/main/java/com/lpvs/service/QueueProcessorService.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022, Samsung Research. All rights reserved.
+ * Copyright (c) 2022, Samsung Electronics Co., Ltd. All rights reserved.
  *
  * Use of this source code is governed by a MIT license that can be
  * found in the LICENSE file.
