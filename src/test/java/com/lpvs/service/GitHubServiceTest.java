6d1b4bbca2a7f3c02e695d4c2aa5f9813e8d3209
Oleksandr Fedorov <o.fedorov@samsung.com>
2022-09-07 20:32:35 +0300

    second test

--- src/test/java/com/lpvs/service/GitHubServiceTest.java
+++ src/test/java/com/lpvs/service/GitHubServiceTest.java
@@ -82,7 +82,6 @@ public class GitHubServiceTest {
 
     @Nested
     class TestGetPullRequestFiles__ApiUrlAbsentPullPresentNoRescan {
-
         final String GH_LOGIN = "test_login";
         final String GH_AUTH_TOKEN = "test_auth_token";
         final String GH_API_URL = "";
@@ -102,11 +101,9 @@ public class GitHubServiceTest {
         String url_pr_2 = "https://api.github.com/repos/Samsung/LPVS/pulls/19";
         String repo_org = "Samsung";
         String repo_name = "LPVS";
-
         String githubFiles = "Projects/Samsung/LPVS/895337e89ae103ff2d18c9e0d93709f743226afa";
         String commit_sha = "895337e89ae103ff2d18c9e0d93709f743226afa";
 
-        // GHCommitPointer mocked_commit_pointer = mock(GHCommitPointer.class);
         @BeforeEach
         void setUp() {
             webhookConfig = new WebhookConfig();
@@ -135,9 +132,6 @@ public class GitHubServiceTest {
             } catch (IOException e) {
                 LOG.error("mocked_repo.getPullRequests error " + e);
             }
-
-//            when(mocked_pr_2.getHead()).thenReturn(mocked_commit_pointer);
-//            when(mocked_commit_pointer.getSha()).thenReturn("895337e89ae103ff2d18c9e0d93709f743226afa");
         }
 
         @Test
@@ -183,24 +177,135 @@ public class GitHubServiceTest {
         }
     }
 
+    @Nested
+    class TestGetPullRequestFiles__ApiUrlPresentPullPresentNoRescan {
+        final String GH_LOGIN = "test_login";
+        final String GH_AUTH_TOKEN = "test_auth_token";
+        String GH_API_URL = "test_api_url";
+        final GitHubService gh_service = new GitHubService(GH_LOGIN, GH_AUTH_TOKEN, GH_API_URL);
+        WebhookConfig webhookConfig;
+        GitHub mocked_instance_gh = mock(GitHub.class);
+        GHRepository mocked_repo = mock(GHRepository.class);
+        PagedIterable<GHPullRequestFileDetail> mocked_list_files = new PagedIterable<GHPullRequestFileDetail>() {
+            @Override
+            public PagedIterator<GHPullRequestFileDetail> _iterator(int i) {
+                return null;
+            }
+        };
+        GHPullRequest mocked_pr_1;
+        GHPullRequest mocked_pr_2;
+        String url_pr_1 = "https://api.github.com/repos/Samsung/LPVS/pulls/18";
+        String url_pr_2 = "https://api.github.com/repos/Samsung/LPVS/pulls/19";
+        String repo_org = "Samsung";
+        String repo_name = "LPVS";
+        String githubFiles = "Projects/Samsung/LPVS/895337e89ae103ff2d18c9e0d93709f743226afa";
+        String commit_sha = "895337e89ae103ff2d18c9e0d93709f743226afa";
+
+        @BeforeEach
+        void setUp() {
+            webhookConfig = new WebhookConfig();
+            webhookConfig.setRepositoryName(repo_name);
+            webhookConfig.setRepositoryOrganization(repo_org);
+            webhookConfig.setAction(PullRequestAction.OPEN);
+            webhookConfig.setPullRequestAPIUrl(url_pr_2);
+            webhookConfig.setHeadCommitSHA(commit_sha);
+
+            try {
+                when(mocked_instance_gh.getRepository(webhookConfig.getRepositoryOrganization() + "/" + webhookConfig.getRepositoryName())).thenReturn(mocked_repo);
+            } catch (IOException e) {
+                LOG.error("mocked_repo.getRepository error " + e);
+            }
+            try {
+                mocked_pr_1 = new GHPullRequestOurMock(
+                        new URL(url_pr_1), null, null, -1, null);
+                mocked_pr_2 = new GHPullRequestOurMock(
+                        new URL(url_pr_2), "GithubService::getRepositoryLicense tests", mocked_list_files, 0, null);
+            } catch (MalformedURLException e) {
+                LOG.error("TestGetPullRequestFiles__ApiUrlAbsentPullPresentNoRescan.setUp() error " + e);
+                fail();
+            }
+            try {
+                when(mocked_repo.getPullRequests(GHIssueState.OPEN)).thenReturn(Arrays.asList(mocked_pr_1, mocked_pr_2));
+            } catch (IOException e) {
+                LOG.error("mocked_repo.getPullRequests error " + e);
+            }
+        }
+
+        @Test
+        public void testGetPullRequestFiles__ApiUrlPresentPullPresentNoRescan() {
+
+            try (MockedStatic<GitHub> mocked_static_gh = mockStatic(GitHub.class); MockedStatic<FileUtil> mocked_static_file_util = mockStatic(FileUtil.class)) {
+                mocked_static_gh.when(() -> GitHub.connectToEnterpriseWithOAuth(GH_API_URL, GH_LOGIN, GH_AUTH_TOKEN)).thenReturn(mocked_instance_gh);
+                mocked_static_file_util.when(() -> FileUtil.saveFiles(mocked_list_files, repo_org + "/" + repo_name, commit_sha, 0))
+                        .thenReturn(githubFiles);
+
+                // main test
+                assertEquals(githubFiles, gh_service.getPullRequestFiles(webhookConfig));
+
+                // verification of calling methods on `Mock`s
+                // `mocked_static_gh` verify
+                mocked_static_gh.verify(() -> GitHub.connectToEnterpriseWithOAuth(GH_API_URL, GH_LOGIN, GH_AUTH_TOKEN), times(1));
+                mocked_static_gh.verifyNoMoreInteractions();
+
+                // `mocked_instance_gh` verify
+                try {
+                    verify(mocked_instance_gh, times(1)).getRepository(webhookConfig.getRepositoryOrganization() + "/" + webhookConfig.getRepositoryName());
+                } catch (IOException e) {
+                    LOG.error("TestGetPullRequestFiles__ApiUrlAbsentPullPresentNoRescan.testGetPullRequestFiles__ApiUrlAbsentPullPresentNoRescan() error " + e);
+                    fail();
+                }
+                verifyNoMoreInteractions(mocked_instance_gh);
+
+                // `mocked_repo` verify
+                try {
+                    verify(mocked_repo, times(1)).getPullRequests(GHIssueState.OPEN);
+                } catch (IOException e) {
+                    LOG.error("TestGetPullRequestFiles__ApiUrlAbsentPullPresentNoRescan.testGetPullRequestFiles__ApiUrlAbsentPullPresentNoRescan() error " + e);
+                    fail();
+                }
+                verifyNoMoreInteractions(mocked_repo);
+
+                // `mocked_static_file_util` verify
+                mocked_static_file_util.verify(
+                        () -> FileUtil.saveFiles(mocked_list_files, repo_org + "/" + repo_name, commit_sha, 0),
+                        times(1));
+                mocked_static_file_util.verifyNoMoreInteractions();
+            }
+        }
+    }
+
 //    @Nested
-//    class TestGetPullRequestFiles__ApiUrlPresentPullPresentNoRescan {
-//
-//        String GH_LOGIN = "test_login";
-//        String GH_AUTH_TOKEN = "test_auth_token";
-//        String GH_API_URL = "test_api_url";
-//        GitHubService gh_service = new GitHubService(GH_LOGIN, GH_AUTH_TOKEN, GH_API_URL);
+//    class TestGetPullRequestFiles__ApiUrlAbsentPullAbsentNoRescan {
+//        final String GH_LOGIN = "test_login";
+//        final String GH_AUTH_TOKEN = "test_auth_token";
+//        final String GH_API_URL = "";
+//        final GitHubService gh_service = new GitHubService(GH_LOGIN, GH_AUTH_TOKEN, GH_API_URL);
 //        WebhookConfig webhookConfig;
 //        GitHub mocked_instance_gh = mock(GitHub.class);
 //        GHRepository mocked_repo = mock(GHRepository.class);
-//        GHLicense mocked_license = mock(GHLicense.class);
-//        String test_license_key = "test_license_key";
+//        PagedIterable<GHPullRequestFileDetail> mocked_list_files = new PagedIterable<GHPullRequestFileDetail>() {
+//            @Override
+//            public PagedIterator<GHPullRequestFileDetail> _iterator(int i) {
+//                return null;
+//            }
+//        };
+//        GHPullRequest mocked_pr_1;
+//        GHPullRequest mocked_pr_2;
+//        String url_pr_1 = "https://api.github.com/repos/Samsung/LPVS/pulls/18";
+//        String url_pr_2 = "https://api.github.com/repos/Samsung/LPVS/pulls/19";
+//        String repo_org = "Samsung";
+//        String repo_name = "LPVS";
+//        String githubFiles = "Projects/Samsung/LPVS/895337e89ae103ff2d18c9e0d93709f743226afa";
+//        String commit_sha = "895337e89ae103ff2d18c9e0d93709f743226afa";
 //
 //        @BeforeEach
 //        void setUp() {
 //            webhookConfig = new WebhookConfig();
-//            webhookConfig.setRepositoryName("LPVS");
-//            webhookConfig.setRepositoryOrganization("Samsung");
+//            webhookConfig.setRepositoryName(repo_name);
+//            webhookConfig.setRepositoryOrganization(repo_org);
+//            webhookConfig.setAction(PullRequestAction.OPEN);
+//            webhookConfig.setPullRequestAPIUrl(url_pr_2);
+//            webhookConfig.setHeadCommitSHA(commit_sha);
 //
 //            try {
 //                when(mocked_instance_gh.getRepository(webhookConfig.getRepositoryOrganization() + "/" + webhookConfig.getRepositoryName())).thenReturn(mocked_repo);
@@ -208,50 +313,68 @@ public class GitHubServiceTest {
 //                LOG.error("mocked_repo.getRepository error " + e);
 //            }
 //            try {
-//                when(mocked_repo.getLicense()).thenReturn(mocked_license);
-//            } catch (IOException e) {
-//                LOG.error("mocked_repo.getLicense error " + e);
+//                mocked_pr_1 = new GHPullRequestOurMock(
+//                        new URL(url_pr_1), null, null, -1, null);
+//                mocked_pr_2 = new GHPullRequestOurMock(
+//                        new URL(url_pr_2), "GithubService::getRepositoryLicense tests", mocked_list_files, 0, null);
+//            } catch (MalformedURLException e) {
+//                LOG.error("TestGetPullRequestFiles__ApiUrlAbsentPullPresentNoRescan.setUp() error " + e);
+//                fail();
 //            }
-//            when(mocked_license.getKey()).thenReturn(test_license_key);
+//            try {
+//                when(mocked_repo.getPullRequests(GHIssueState.OPEN)).thenReturn(Arrays.asList(mocked_pr_1, mocked_pr_2));
+//            } catch (IOException e) {
+//                LOG.error("mocked_repo.getPullRequests error " + e);
+//            }
 //        }
 //
 //        @Test
-//        public void testGetPullRequestFiles__ApiUrlPresentPullPresentNoRescan() {
+//        public void testGetPullRequestFiles__ApiUrlAbsentPullAbsentNoRescan() {
 //
-//            try (MockedStatic<GitHub> mocked_static_gh = mockStatic(GitHub.class)) {
-//                mocked_static_gh.when(() -> GitHub.connectToEnterpriseWithOAuth(GH_API_URL, GH_LOGIN, GH_AUTH_TOKEN)).thenReturn(mocked_instance_gh);
+//            try (MockedStatic<GitHub> mocked_static_gh = mockStatic(GitHub.class); MockedStatic<FileUtil> mocked_static_file_util = mockStatic(FileUtil.class)) {
+//                mocked_static_gh.when(() -> GitHub.connect(GH_LOGIN, GH_AUTH_TOKEN)).thenReturn(mocked_instance_gh);
+//                mocked_static_file_util.when(() -> FileUtil.saveFiles(mocked_list_files, repo_org + "/" + repo_name, commit_sha, 0))
+//                        .thenReturn(githubFiles);
 //
 //                // main test
-//                assertEquals(test_license_key, gh_service.getRepositoryLicense(webhookConfig));
+//                assertEquals(githubFiles, gh_service.getPullRequestFiles(webhookConfig));
 //
 //                // verification of calling methods on `Mock`s
 //                // `mocked_static_gh` verify
-//                mocked_static_gh.verify(() -> GitHub.connectToEnterpriseWithOAuth(GH_API_URL, GH_LOGIN, GH_AUTH_TOKEN), times(1));
+//                mocked_static_gh.verify(() -> GitHub.connect(GH_LOGIN, GH_AUTH_TOKEN), times(1));
 //                mocked_static_gh.verifyNoMoreInteractions();
 //
 //                // `mocked_instance_gh` verify
 //                try {
 //                    verify(mocked_instance_gh, times(1)).getRepository(webhookConfig.getRepositoryOrganization() + "/" + webhookConfig.getRepositoryName());
 //                } catch (IOException e) {
-//                    LOG.error("mocked_instance_gh.getRepository error " + e);
+//                    LOG.error("TestGetPullRequestFiles__ApiUrlAbsentPullPresentNoRescan.testGetPullRequestFiles__ApiUrlAbsentPullPresentNoRescan() error " + e);
+//                    fail();
 //                }
 //                verifyNoMoreInteractions(mocked_instance_gh);
 //
 //                // `mocked_repo` verify
 //                try {
-//                    verify(mocked_repo, times(1)).getLicense();
+//                    verify(mocked_repo, times(1)).getPullRequests(GHIssueState.OPEN);
 //                } catch (IOException e) {
-//                    LOG.error("mocked_repo.getLicense error " + e);
+//                    LOG.error("TestGetPullRequestFiles__ApiUrlAbsentPullPresentNoRescan.testGetPullRequestFiles__ApiUrlAbsentPullPresentNoRescan() error " + e);
+//                    fail();
 //                }
 //                verifyNoMoreInteractions(mocked_repo);
 //
-//                // `mocked_license` verify
-//                verify(mocked_license, times(1)).getKey();
-//                verifyNoMoreInteractions(mocked_license);
+//                // `mocked_static_file_util` verify
+//                mocked_static_file_util.verify(
+//                        () -> FileUtil.saveFiles(mocked_list_files, repo_org + "/" + repo_name, commit_sha, 0),
+//                        times(1));
+//                mocked_static_file_util.verifyNoMoreInteractions();
 //            }
 //        }
 //    }
 
+// GHCommitPointer mocked_commit_pointer = mock(GHCommitPointer.class);
+
+//            when(mocked_pr_2.getHead()).thenReturn(mocked_commit_pointer);
+//            when(mocked_commit_pointer.getSha()).thenReturn("895337e89ae103ff2d18c9e0d93709f743226afa");
     @Nested
     class TestGetRepositoryLicense__ApiUrlAbsentLisencePresent {
 
