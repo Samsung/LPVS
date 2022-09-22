b1fc2999bf6a7b419c98dcbe5bb22a240ecefe7a
Oleg Kopysov <o.kopysov@samsung.com>
2022-09-22 10:01:14 +0300

    Update ResponseWrapper.java

--- src/main/java/com/lpvs/entity/ResponseWrapper.java
+++ src/main/java/com/lpvs/entity/ResponseWrapper.java
@@ -13,12 +13,4 @@ public class ResponseWrapper {
     public ResponseWrapper(String message) {
         this.message = message;
     }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
