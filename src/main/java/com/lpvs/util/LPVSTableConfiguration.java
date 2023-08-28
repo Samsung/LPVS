c2f49ae3f7f3b9c92479dc4e3d5e800f2ff6b3fa
m-rudyk <121865672+m-rudyk@users.noreply.github.com>
2023-08-28 11:52:57 +0300

    Add table configuration class (#196)
    * feat: add table configuration class
    
    * fix: apply remarks after review

---
+++ src/main/java/com/lpvs/util/LPVSTableConfiguration.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2023, Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
+package com.lpvs.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LPVSTableConfiguration {
+    @Value("${app.table.detectedLicenseName}")
+    private String detectedLicenseName;
+
+    @Value("${app.table.detectedLicenseSchema}")
+    private String detectedLicenseSchema;
+
+    @Value("${app.table.diffFileName}")
+    private String diffFileName;
+
+    @Value("${app.table.pullRequestsName}")
+    private String pulLRequestsName;
+
+    @Value("${app.table.queueName}")
+    private String queueName;
+
+    // Configuration for detected license name
+    public String getDetectedLicenseName() {
+        return detectedLicenseName;
+    }
+
+    // Configuration for detected license schema
+    public String getDetectedLicenseSchema() {
+        return detectedLicenseSchema;
+    }
+
+    // Configuration for diff file name table
+    public String getDiffFileName() {
+        return diffFileName;
+    }
+
+    // Configuration for pull request name table
+    public String getPullRequestsName() {
+        return pulLRequestsName;
+    }
+
+    // Configurations for queue name
+    public String getQueueName() {
+        return queueName;
+    }
+}
