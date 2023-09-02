1c310afcb4bd42c3ce78d86a8fefa3653a06c760
Hankyuhyun <khh5345@naver.com>
2023-09-02 17:00:30 +0900

    drop member table if member table exists.
    Signed-off-by: Hankyuhyun <khh5345@naver.com>

--- src/main/resources/database_dump.sql
+++ src/main/resources/database_dump.sql
@@ -159,6 +159,9 @@ CREATE TABLE `queue` (
 -- Table structure for table `member`
 --
 
+DROP TABLE IF EXISTS `member`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `member` (
   `member_id` bigint(20) PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `e-mail` varchar(255) NOT NULL,
