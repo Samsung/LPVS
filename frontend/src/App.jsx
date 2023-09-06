7d042ab1f2bca54d046711c44f5ef44dfd56d771
Han Kyuhyun <khh5345@naver.com>
2023-09-06 20:33:10 +0900

    Add Login Page.
    Signed-off-by: Han Kyuhyun <khh5345@naver.com>
    Co-authored-by: ByunJeongHeum <quswjdgma83@naver.com>
    Co-authored-by: June Saehwan <shjune5180@naver.com>
    Co-authored-by: Jongmin Lee <rjsqktod1@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </BrowserRouter>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,57 +5,66 @@
  * found in the LICENSE file.
  */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import "../css/Home_style.css";
 
 export const Home = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    axios.get("/login/check").then((loginresponse) => {
+      if (loginresponse.data.isLoggedIn) {
+        setIsLoggedIn(loginresponse.data.isLoggedIn);
+        axios.get("/user/info").then((userInfoResponse) => {
+          setUsername(userInfoResponse.data);
+        });
+      }
+    });
+  }, []);
 
   return (
     <div className="home">
       <div className="div">
         <div className="LPVS-info">
           <div className="overlap-group">
-            <p className="open-source-code">
-              <span className="text-wrapper">
-                  Welcome to the License Pre-Validation Service (LPVS).
-    			<h3>Usage Procedure</h3>
-    			<ol>
-        		<li>Sign up and Login to the service.</li>
-        		<li>Go to user information page, enter your GitHub ID (required) and Organization Name (optional), then click the "Admit" button.</li>
-        		<li>Login to GitHub using the GitHub ID you entered in step 2.</li>
-        		<li>To configure the repository for license validation, follow these steps:
-            			<ol type="a">
-                		<li>Go to the repository you want to validation.</li>
-                		<li>Navigate to Settings -{">"} Webhooks -{">"} Add Webhooks.</li>
-                		<li>Enter 'http://{"<"}IP where LPVS is running:7896/webhooks{">"}' in the Payload URL field.</li>
-                		<li>Select 'application/json' for Content Type.</li>
-                		<li>Enter 'LPVS' in the Secret field.</li>
-                		<li>Under "Which events would you like to trigger this webhook?", select 'Let me select individual events.' and check only the 'Pull Request' option.</li>
-                		<li>Click the green 'Add webhook' button.</li>
-            			</ol>
-        		</li>
-        		<li>After completing the webhook setup for the repository, create a Pull Request on that repository to see the results of license validation.</li>
-    			</ol>
-
-    			<h3>Important Notes</h3>
-    			<ul>
-        		<li>If you enter a GitHub ID that is already used by someone else in your user information, it will not be reflected.</li>
-        		<li>This service is only available for Public Repository.</li>
-        		<li>Webhook settings are mandatory for using this service.</li>
-    			</ul>
-              </span>
-            </p>
+            <div className="user-guide">
+              <p>Welcome to the License Pre-Validation Service (LPVS).</p>
+              <h3>Usage Procedure</h3>
+              <ol>
+                <li>Sign up and Login to the service.</li>
+                <li>Go to user information page, enter your GitHub ID (required) and Organization Name (optional), then click the "Admit" button.</li>
+                <li>Login to GitHub using the GitHub ID you entered in step 2.</li>
+                <li>To configure the repository for license validation, follow these steps:</li>
+                <ol type="a">
+                  <li>Go to the repository you want to validation.</li>
+                  <li>Navigate to Settings -{">"} Webhooks -{">"} Add Webhooks.</li>
+                  <li>Enter 'http://{"<"}IP where LPVS is running:7896/webhooks{">"}' in the Payload URL field.</li>
+                  <li>Select 'application/json' for Content Type.</li>
+                  <li>Enter 'LPVS' in the Secret field.</li>
+                  <li>Under "Which events would you like to trigger this webhook?", select 'Let me select individual events.' and check only the 'Pull Request' option.</li>
+                  <li>Click the green 'Add webhook' button.</li>
+                </ol>
+                <li>After completing the webhook setup for the repository, create a Pull Request on that repository to see the results of license validation.</li>
+              </ol>
+              <h3>Important Notes</h3>
+              <ul>
+                <li>If you enter a GitHub ID that is already used by someone else in your user information, it will not be reflected.</li>
+                <li>This service is only available for Public Repository.</li>
+                <li>Webhook settings are mandatory for using this service.</li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="LPVS-logo">
           <a href="https://github.com/Samsung/LPVS">
-            <img className="LPVS-remove" alt="Lpvs remove" src="/image/LPVSLogo.png" />
+            <img className="LPVS-github" alt="img" src="/image/png/LPVSLogo.png" />
           </a>
-          <div className="license-pre">
+          <div className="license-explain">
             License <br />
-            Pre
-            <br />
+            Pre <br />
             Validation <br />
             Service
           </div>
@@ -63,9 +72,38 @@ export const Home = () => {
         </div>
         <div className="menubar-top">
           <div className="menu-line" />
+          <div className="menu">
+            <div className="overlap">
+              <div className="profile-border" />
+              <div className="profile">
+                <div className="overlap-group-2">
+                  <img className="image" alt="img" src="/image/png/ProfileImg.png" />
+                  <div className="text-wrapper-3">
+                    {isLoggedIn ? (
+                      <span style={{ color: "black", textDecoration: "none" }}>
+                        <Link
+                          to={"/home"}
+                          style={{ color: "black", textDecoration: "none" }}
+                        >
+                          {username.name}
+                        </Link>
+                      </span>
+                    ) : (
+                      <Link
+                        to={"/login"}
+                        style={{ color: "black", textDecoration: "none" }}
+                      >
+                        Login
+                      </Link>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
           <Link to={"/home"} style={{ color: "black", textDecoration: "none"}}>
-          <img className="LPVS" alt="Lpvs" src="/image/LPVS_logo_bar.png" />
-            </Link>
+            <img className="LPVS" alt="img" src="/image/png/LPVS_logo_bar.png" />
+          </Link>
         </div>
       </div>
     </div>
