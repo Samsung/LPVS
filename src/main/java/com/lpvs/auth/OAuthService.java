88dbaa39d91399c644dcae8e46fb978bbf2cf6b9
Han Kyuhyun <khh5345@naver.com>
2023-09-11 18:53:12 +0900

    Remove unnecessary comment.
    Signed-off-by: Han Kyuhyun <khh5345@naver.com>
    Co-authored-by: ByunJeongHeum <quswjdgma83@naver.com>
    Co-authored-by: June Saehwan <shjune5180@naver.com>
    Co-authored-by: Jongmin Lee <rjsqktod1@gmail.com>

--- src/main/java/com/lpvs/auth/OAuthService.java
+++ src/main/java/com/lpvs/auth/OAuthService.java
@@ -69,7 +69,7 @@ public class OAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2
     private LPVSMember saveOrUpdate(MemberProfile memberProfile) {
 
         LPVSMember LPVSMember = LPVSMemberRepository.findByEmailAndProvider(memberProfile.getEmail(), memberProfile.getProvider())
-                .map(m -> m.update(memberProfile.getName(), memberProfile.getEmail())) // OAuth 서비스 사이트에서 유저 정보 변경이 있을 수 있기 때문에 우리 DB에도 update
+                .map(m -> m.update(memberProfile.getName(), memberProfile.getEmail()))
                 .orElse(memberProfile.toMember());
 
         return LPVSMemberRepository.save(LPVSMember);
