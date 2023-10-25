54907e0984ff575e89ef698def63c7c2281afeb0
v-kerimov <112548427+v-kerimov@users.noreply.github.com>
2023-10-25 13:31:57 +0300

    Added new tests for LPVSStatisticsService (#260)
    * New Statistics service tests
    
    * New Statistics service tests
    
    * Increased LPVSScanossDetectedService test coverage
    
    * Unnecessary change to pom.xml reverted
    
    * Removed unnecessary comment
    
    Signed-off-by: Vasyl Kerimov <v.kerimov@samsung.com>
    
    ---------
    
    Signed-off-by: Vasyl Kerimov <v.kerimov@samsung.com>

--- src/test/java/com/lpvs/service/LPVSStatisticsServiceTest.java
+++ src/test/java/com/lpvs/service/LPVSStatisticsServiceTest.java
@@ -6,18 +6,27 @@
  */
 package com.lpvs.service;
 
+import com.lpvs.entity.LPVSDetectedLicense;
+import com.lpvs.entity.LPVSLicense;
 import com.lpvs.entity.LPVSMember;
 import com.lpvs.entity.LPVSPullRequest;
+import com.lpvs.entity.dashboard.Dashboard;
 import com.lpvs.entity.enums.Grade;
+import com.lpvs.exception.WrongAccessException;
+import com.lpvs.repository.LPVSDetectedLicenseRepository;
+import com.lpvs.repository.LPVSLicenseRepository;
 import com.lpvs.repository.LPVSPullRequestRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.core.Authentication;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,6 +43,12 @@ public class LPVSStatisticsServiceTest {
 
     @Mock
     private LPVSLoginCheckService loginCheckService;
+
+    @Mock
+    private LPVSLicenseRepository licenseRepository;
+
+    @Mock
+    private LPVSDetectedLicenseRepository detectedLicenseRepository;
 
     @BeforeEach
     public void setUp() {
@@ -77,6 +92,48 @@ public class LPVSStatisticsServiceTest {
 
         assertNotNull(result);
         assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testPathCheckWrongPathException() {
+        Authentication authentication = mock(Authentication.class);
+        LPVSMember member = new LPVSMember();
+        member.setNickname("testNickname");
+        when(loginCheckService.getMemberFromMemberMap(authentication)).thenReturn(member);
+        when(pullRequestRepository.findBySenderOrPullRequestHead("testNickname")).thenReturn(Collections.emptyList());
+        Exception exception = assertThrows(WrongAccessException.class, () -> {
+            statisticsService.pathCheck("wrongpath", "testNickname", authentication);
+        });
+        assertEquals("WrongPathException", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"81 ", "51 ", "31 ", "16 "})
+    public void testGetDashboardEntity(String match) {
+        Authentication authentication = mock(Authentication.class);
+        LPVSMember member = new LPVSMember();
+        LPVSPullRequest mockRequest = mock(LPVSPullRequest.class);
+        List<LPVSPullRequest> pullRequests = new ArrayList<>(){{ add(mockRequest); }};
+        LPVSDetectedLicense detectedLicense = new LPVSDetectedLicense(){{
+            setMatch(match);
+            setLicense(new LPVSLicense(){{
+                setSpdxId("MIT");
+            }});
+            setIssue(true);
+        }};
+        member.setNickname("testNickname");
+        when(loginCheckService.getMemberFromMemberMap(authentication)).thenReturn(member);
+        when(pullRequestRepository.findByPullRequestBase("testNickname")).thenReturn(pullRequests);
+        when(licenseRepository.takeAllSpdxId()).thenReturn(Collections.singletonList("MIT"));
+        when(mockRequest.getDate()).thenReturn(new Date());
+        when(mockRequest.getSender()).thenReturn("");
+        when(mockRequest.getRepositoryName()).thenReturn("name");
+        when(detectedLicenseRepository.findNotNullDLByPR(mockRequest)).thenReturn(new ArrayList<>(){{
+            add(detectedLicense);
+        }});
+
+        Dashboard result = statisticsService.getDashboardEntity("own", "testNickname", authentication);
+        assertNotNull(result);
     }
 
     @Test
