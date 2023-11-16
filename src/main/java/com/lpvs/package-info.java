814f7adf8524e67ea4cb6a4cb7e5f1738980c698
Oleg Kopysov <o.kopysov@samsung.com>
2023-11-16 16:07:01 +0200

    fix: Add missing license headers
    Signed-off-by: Oleg Kopysov <o.kopysov@samsung.com>

--- src/main/java/com/lpvs/package-info.java
+++ src/main/java/com/lpvs/package-info.java
@@ -1,4 +1,11 @@
 /**
+ * Copyright (c) 2023, Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
+/**
  * Provides the main classes and components for the License Pre-Validation Service (LPVS) application.
  * LPVS is an open-source tool designed to mitigate license-related risks in open-source projects.
  * It analyzes the project, identifies components and licenses, and generates potential issue cases.
