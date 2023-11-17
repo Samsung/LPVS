78537e34d22e88fbc361324f2a2cc12095b00b82
Oleg Kopysov <o.kopysov@samsung.com>
2023-11-17 14:31:17 +0200

    fix: Add missing license and copyright statements
    Signed-off-by: Oleg Kopysov <o.kopysov@samsung.com>

--- src/main/java/com/lpvs/entity/result/LPVSResultInfo.java
+++ src/main/java/com/lpvs/entity/result/LPVSResultInfo.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright 2023 Basaeng, kyudori, hwan5180, quswjdgma83
+ *
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
 package com.lpvs.entity.result;
 
 import lombok.AllArgsConstructor;
