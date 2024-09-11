0bd3d82d9531e40957596ac2672e15742aa459e1
Oleg Kopysov <o.kopysov@samsung.com>
2024-09-11 14:00:25 +0300

    chore: Update copyrights year
    Signed-off-by: Oleg Kopysov <o.kopysov@samsung.com>

--- src/main/java/com/lpvs/controller/LPVSWebController.java
+++ src/main/java/com/lpvs/controller/LPVSWebController.java
@@ -1,5 +1,6 @@
 /**
- * Copyright 2023 Basaeng, kyudori, hwan5180, quswjdgma83
+ * Copyright (с) 2023, Basaeng, kyudori, hwan5180, quswjdgma83
+ * Copyright (c) 2024, Samsung Electronics Co., Ltd. All rights reserved.
  *
  * Use of this source code is governed by a MIT license that can be
  * found in the LICENSE file.
